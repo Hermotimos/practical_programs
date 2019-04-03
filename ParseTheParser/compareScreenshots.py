110f46cc3e85cab88eae7b486f3fd76cc1d731c5
Lukasz <lukas.kozicki@gmail.com>
2019-04-03 11:53:03 +0200

    functions screenshot_1920x1080() created, get_image_data adjusted

--- ParseTheParser/compareScreenshots.py
+++ ParseTheParser/compareScreenshots.py
@@ -10,18 +10,27 @@ from data import NUMBERS_LIST
 # print(get_image_dimensions('example.png'))
 
 
-def get_image_data(image):
+def screenshot_1920x1080(up_from_left=0, up_from_top=0, d_from_left=1920, d_from_top=1080):
+    screenshot = pyautogui.screenshot(region=(up_from_left, up_from_top, d_from_left, d_from_top))
+    width = d_from_left - up_from_left
+    height = d_from_top - up_from_top
+    return screenshot, (width, height)
 
+
+def get_image_data(image_with_size=()):
+    width, height = image_with_size[1]
     img_data = {}
-    for wdth_pix in range(10):
-        for lngth_pix in range(23):
-            val = image.getpixel((wdth_pix, lngth_pix))
+    for wdth_pix in range(width+1):
+        for lngth_pix in range(height+1):
+            val = image_with_size[0].getpixel((wdth_pix, lngth_pix))
             img_data[(wdth_pix, lngth_pix)] = val
     return img_data
 
-
-screenshot = pyautogui.screenshot(region=(465, 51, 474, 73))
-print(get_image_data(screenshot))
+# # Test of functions screenshot_1920x1080 + get_image_data: for 0 recognition (passed)
+# screenshot = screenshot_1920x1080(465, 51, 474, 73)
+# print(screenshot)
+# print(get_image_data(screenshot))
+# print(get_image_data(screenshot) == NUMBERS_LIST[0])
 
 
 def compare_images(img_data1, img_data2):
@@ -40,8 +49,8 @@ def recognize_number(scrnshot):
     return result
 
 
-screen_now = pyautogui.screenshot(region=(465, 51, 474, 73))
-print(recognize_number(screen_now))
+# screen_now = pyautogui.screenshot(region=(465, 51, 474, 73))
+# print(recognize_number(screen_now))
 
 
 
