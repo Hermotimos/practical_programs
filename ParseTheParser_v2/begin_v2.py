9350c08972154a6d04be36fb6305b7bcf2e8dc10
Lukasz <lukas.kozicki@gmail.com>
2019-04-04 10:21:07 +0200

    prompt message changed

--- ParseTheParser_v2/begin_v2.py
+++ ParseTheParser_v2/begin_v2.py
@@ -1,10 +1,10 @@
 
 def ask_confirm():
     confirm = input('\n\n!!! PREPARE YOUR PARSER !!!\n\n'
-                    '1. TYPE OF SENTENCES TO BROWSE.\n'
-                    '2. END DATE\n'
+                    '1. CHOSE TYPE OF SENTENCES TO BROWSE.\n'
+                    '2. SET END DATE\n'
                     '3. SET "without thesis" AND "with justification" fields.\n'
-                    '4. NUMBER OF LAST PAGE = 1\n\n'
+                    '4. SET NUMBER OF LAST PAGE = 1\n\n'
                     'Enter number of pages to browse or leave empty for 10000:\n'
                     'CONFIRM: ENTER ===> you will then have 5 secs to switch to the Parser window.\n')
     if confirm == '':
