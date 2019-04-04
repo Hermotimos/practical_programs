f159492058d0f3b1edf7531c90a3e5f7b2c0ad68
Lukasz <lukas.kozicki@gmail.com>
2019-04-04 09:31:55 +0200

    ask_confirm() moved to separate file

---
+++ ParseTheParser_v2/begin.py
@@ -0,0 +1,18 @@
+
+def ask_confirm():
+    confirm = input('\n\n!!! PREPARE YOUR PARSER !!!\n\n'
+                    '1. TYPE OF SENTENCES TO BROWSE.\n'
+                    '2. END DATE\n'
+                    '3. SET "without thesis" AND "with justification" fields.\n'
+                    '4. NUMBER OF LAST PAGE = 1\n\n'
+                    'Enter number of pages to browse or leave empty for 10000:\n'
+                    'CONFIRM: ENTER ===> you will then have 5 secs to switch to the Parser window.\n')
+    if confirm == '':
+        confirm = 10000
+    try:
+        confirm = int(confirm)
+        assert confirm > 0
+        return confirm
+    except Exception as e:
+        print(e)
+        return ask_confirm()
--- ParseTheParser_v2/main_v2.py
+++ ParseTheParser_v2/main_v2.py
@@ -1,25 +1,7 @@
-from movement_and_clicks_v2 import autoparse
-import pyautogui
 import time
-
-
-def ask_confirm():
-    confirm = input('\n\n!!! PREPARE YOUR PARSER !!!\n\n'
-                    '1. TYPE OF SENTENCES TO BROWSE.\n'
-                    '2. END DATE\n'
-                    '3. SET "without thesis" AND "with justification" fields.\n'
-                    '4. NUMBER OF PAGES = 1\n\n'
-                    'Enter number of pages to browse or leave empty for 10000:\n'
-                    'CONFIRM: ENTER ===> you will then have 5 secs to switch to the Parser window.\n')
-    if confirm == '':
-        confirm = 10000
-    try:
-        confirm = int(confirm)
-        assert confirm > 0
-        return confirm
-    except Exception as e:
-        print(e)
-        return ask_confirm()
+import pyautogui
+from movement_and_clicks_v2 import autoparse
+from begin import ask_confirm
 
 
 pages = ask_confirm()
@@ -29,9 +11,3 @@ try:
 except pyautogui.FailSafeException:
     print("FAILSAFE-ESCAPED.")
 
-
-
-# TODO change of program window size seems to change pixels in numbers data
-# Sol1 - measure accuracy
-# Sol2 - full screen only [add to program]
-# Sol3 - look for one
