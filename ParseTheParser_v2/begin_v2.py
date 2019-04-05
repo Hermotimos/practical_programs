08f0e21e6293cdfd47a116fb784376f3cd83992b
Lukasz <lukas.kozicki@gmail.com>
2019-04-05 09:01:09 +0200

    removed default 10000 for number of pages to browse

--- ParseTheParser_v2/begin_v2.py
+++ ParseTheParser_v2/begin_v2.py
@@ -5,10 +5,8 @@ def ask_confirm():
                     '2. SET END DATE\n'
                     '3. SET "without thesis" AND "with justification" fields.\n'
                     '4. SET NUMBER OF LAST PAGE = 1\n\n'
-                    'Enter number of pages to browse or leave empty for 10000:\n'
+                    'Enter number of pages to browse:\n'
                     'CONFIRM: ENTER ===> you will then have 5 secs to switch to the Parser window.\n')
-    if confirm == '':
-        confirm = 10000
     try:
         confirm = int(confirm)
         assert confirm > 0
