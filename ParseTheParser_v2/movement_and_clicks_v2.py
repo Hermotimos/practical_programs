144b4f6943ae27b7cc01751bb25a5b7c45ae2268
Lukasz <lukas.kozicki@gmail.com>
2019-04-05 15:38:12 +0200

    correcting features commented out (may be useful in the future, but now they cause huge lags)

--- ParseTheParser_v2/movement_and_clicks_v2.py
+++ ParseTheParser_v2/movement_and_clicks_v2.py
@@ -6,12 +6,12 @@ from image_processing_v2 import await_image, try_go_to_image, recognize_number
 pyautogui.PAUSE = 0.1
 pyautogui.FAILSAFE = True
 
-IMG_HOMEPAGE = '.\\IMG_HOMEPAGE.png'
-IMG_ZTEZA = '.\\IMG_ZTEZA.png'
-IMG_ZUZASAD = '.\\IMG_ZUZASAD.png'
-IMG_RODZAJ = '.\\IMG_RODZAJ.png'
-IMG_WYROK = '.\\IMG_WYROK.png'
-IMG_NROSTAT = '.\\IMG_NROSTAT.png'
+# IMG_HOMEPAGE = '.\\IMG_HOMEPAGE.png'
+# IMG_ZTEZA = '.\\IMG_ZTEZA.png'
+# IMG_ZUZASAD = '.\\IMG_ZUZASAD.png'
+# IMG_RODZAJ = '.\\IMG_RODZAJ.png'
+# IMG_WYROK = '.\\IMG_WYROK.png'
+# IMG_NROSTAT = '.\\IMG_NROSTAT.png'
 IMG_STATUS = '.\\IMG_STATUS.png'
 IMG_SZUKAJ = '.\\IMG_SZUKAJ.png'
 IMG_START_BLACK = '.\\IMG_START_BLACK.png'
@@ -28,7 +28,7 @@ def autoparse(pages_to_browse):
     new_cnt = 0
 
     print('-' * 20, 'START', '-' * 20)
-    correct_settings()
+    # correct_settings()
     start_browsing()
 
     while pages_to_browse > 0:
@@ -50,56 +50,57 @@ def autoparse(pages_to_browse):
     finish_browsing(new_cnt)
 
 
-def correct_settings():
-
-    def correct_homepage():
-        t = time.time()
-        try_go_to_image(IMG_HOMEPAGE)
-        pyautogui.click()
-        print('\t{}s'.format(round(time.time() - t), 0))
-
-    def correct_zteza():
-        t = time.time()
-        is_zteza_none = await_image(IMG_ZTEZA, 1)
-        if is_zteza_none:
-            try_go_to_image(IMG_ZTEZA)
-            pyautogui.move(90, 0)
-            pyautogui.click()
-            print('\t{}s'.format(round(time.time() - t), 0))
-
-    def correct_zuzasad():
-        t = time.time()
-        is_zuzasad_none = await_image(IMG_ZUZASAD, 1)
-        if is_zuzasad_none:
-            try_go_to_image(IMG_ZUZASAD)
-            pyautogui.move(30, 0)
-            pyautogui.click()
-            print('\t{}s'.format(round(time.time() - t), 0))
-
-    def correct_rodzaj():
-        t = time.time()
-        is_rodzaj_none = await_image(IMG_RODZAJ, 1)
-        if is_rodzaj_none:
-            try_go_to_image(IMG_RODZAJ)
-            pyautogui.click()
-            try_go_to_image(IMG_WYROK)
-            pyautogui.click()
-            print('\t{}s'.format(round(time.time() - t), 0))
-
-    def correct_nrostat():
-        t = time.time()
-        try_go_to_image(IMG_NROSTAT)
-        pyautogui.move(0, 20)
-        pyautogui.click()
-        pyautogui.press('delete', presses=5)
-        pyautogui.typewrite('1')
-        print('\t{}s'.format(round(time.time() - t), 0))
-
-    correct_homepage()
-    correct_zteza()
-    correct_zuzasad()
-    correct_rodzaj()
-    correct_nrostat()
+# These proved to cause more problems than solve
+# def correct_settings():
+#
+#     def correct_homepage():
+#         t = time.time()
+#         try_go_to_image(IMG_HOMEPAGE)
+#         pyautogui.click()
+#         print('\t{}s'.format(round(time.time() - t), 0))
+#
+#     def correct_zteza():
+#         t = time.time()
+#         is_zteza_none = await_image(IMG_ZTEZA, 1)
+#         if is_zteza_none:
+#             try_go_to_image(IMG_ZTEZA)
+#             pyautogui.move(90, 0)
+#             pyautogui.click()
+#             print('\t{}s'.format(round(time.time() - t), 0))
+#
+#     def correct_zuzasad():
+#         t = time.time()
+#         is_zuzasad_none = await_image(IMG_ZUZASAD, 1)
+#         if is_zuzasad_none:
+#             try_go_to_image(IMG_ZUZASAD)
+#             pyautogui.move(30, 0)
+#             pyautogui.click()
+#             print('\t{}s'.format(round(time.time() - t), 0))
+#
+#     def correct_rodzaj():
+#         t = time.time()
+#         is_rodzaj_none = await_image(IMG_RODZAJ, 1)
+#         if is_rodzaj_none:
+#             try_go_to_image(IMG_RODZAJ)
+#             pyautogui.click()
+#             try_go_to_image(IMG_WYROK)
+#             pyautogui.click()
+#             print('\t{}s'.format(round(time.time() - t), 0))
+#
+#     def correct_nrostat():
+#         t = time.time()
+#         try_go_to_image(IMG_NROSTAT)
+#         pyautogui.move(0, 20)
+#         pyautogui.click()
+#         pyautogui.press('delete', presses=5)
+#         pyautogui.typewrite('1')
+#         print('\t{}s'.format(round(time.time() - t), 0))
+#
+#     correct_homepage()
+#     correct_zteza()
+#     correct_zuzasad()
+#     correct_rodzaj()
+#     correct_nrostat()
 
 
 def start_browsing():
