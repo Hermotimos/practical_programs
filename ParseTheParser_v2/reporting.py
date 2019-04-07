ec6c8a9086934d45e736d402ce79b046185bc258
Lukasz <lukas.kozicki@gmail.com>
2019-04-07 22:43:23 +0200

    rename

--- ParseTheParser_v2/reporting.py
+++ ParseTheParser_v2/reporting.py
@@ -3,7 +3,7 @@ import datetime
 from movement_and_clicks_v2 import click_next
 
 
-def report(called_function):
+def call_and_report(called_function):
     print('[{}] '.format(datetime.datetime.now().strftime('%H:%M:%S')), end='')
     print(called_function.__repr__().split()[1])
     called_function()
@@ -12,6 +12,6 @@ def report(called_function):
 # but maybe only because the ones called stll have older reporting from within...
 
 
-report(click_next)
+call_and_report(click_next)
 
 
